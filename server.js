03c5dfe68aa1cae5cb4b596b5aa353e74fdc06aa
Vagner Jorge Souza Feitosa <svagnerjorge@gmail.com>
2025-10-28 21:23:59 -0300

    .

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import mysql from "mysql2";
+import bodyParser from "body-parser";
+import cors from "cors";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 8000;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "techor_db",
+});
+ 
+
+db.connect(err => {
+  if (err) {
+    console.error("❌ Erro ao conectar ao MySQL:", err);
+    return;
+  }
+  console.log("✅ Conectado ao MySQL!");
+});
+
+// Rota para receber o formulário
+app.post("/enviar", (req, res) => {
+  const { nome, email, mensagem } = req.body;
+  const sql = "INSERT INTO contatos (nome, email, mensagem) VALUES (?, ?, ?)";
+  db.query(sql, [nome, email, mensagem], err => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Erro ao salvar mensagem 😢");
+    } else {
+      res.send("Mensagem enviada com sucesso 💚");
+    }
+  });
+});
+
 
 // Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, "public")));
